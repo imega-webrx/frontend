e873b9d47abcaaa4b29e278136a9ee72430b5a1c
Dmitry Stoletov <irvis@imega.ru>
2021-03-01 00:02:45 +0300

    set backend host
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- src/components/results/resultsWrapper.js
+++ src/components/results/resultsWrapper.js
@@ -1,79 +1,35 @@
-import  React from "react";
-import Intro from "./intro";
-import SuggestedResults from "../results/suggestedResults";
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import {
-
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
-
-
-/*
-const getData = (drugname) => {
-  let pharmacyRes;
-  return fetch('http://localhost:5000/pharmacy')
-    .then(res => {
-      return res.json().then(data => {
-        return data;
-      })
-    })
-}
-*/
-/*
-const getData = async (drugname) => {
-  let result = await fetch('http://localhost:5000/pharmacy');
-  let json =  await result.json();
-  return await json[0][drugname];
-}
-
-*/
-class ResultsWrapper extends React.Component{
-constructor (props) {
-    super(props);
-    this.state = {
-    vol:[], 
-      }
+class ResultsWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            vol: [],
+        };
     }
 
+    componentDidMount() {
+        axios
+            .post("https://webrx/api/col", { name: this.props.match.params.id })
+            .then((res) => {
+                console.log(res.data);
+                this.setState({ vol: res.data });
+            });
+    }
 
+    render() {
+        const listItems = this.state.vol.map((number) => (
+            <li>{number.name}</li>
+        ));
 
-componentDidMount(){
+        let id = this.props.match.params.id;
 
-
-axios.post('http://localhost:8000/col',{name:this.props.match.params.id}).then(res => {
-       console.log(res.data);
-this.setState({vol:res.data});
-  
-
-      })
-
-
-  
+        return (
+            <div>
+                <li>{listItems}</li>
+            </div>
+        );
+    }
 }
-
-
-render(){
-const listItems = this.state.vol.map((number) =>
-  <li>{number.name}</li>
-);
-
-
-let id = this.props.match.params.id;
-
-return(
-<div>
-<li>
-{listItems}
-</li>
-</div>
-
-  )
-
-}
-
-}
- export default ResultsWrapper;
+export default ResultsWrapper;
