5a8ffb92aa96a2407e9c6125b86634f229a104f9
shemmmil <shemmmil1996@gmail.com>
2021-03-13 17:13:18 +0300

    refact: formatting styles

--- src/Layout/Sections/Newsletter/index.js
+++ src/Layout/Sections/Newsletter/index.js
@@ -37,28 +37,74 @@ const Newsletter = () => (
 );
 
 const Container = tw("div")`bg-yellow-300`;
-const Section = tw(
-    "section"
-)`max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center`;
+const Section = tw("section")`
+    max-w-7xl
+    mx-auto
+    py-12 px-4
+    sm:px-6
+    lg:py-16
+    lg:px-8
+    lg:flex
+    lg:items-center
+`;
 
 const InfoLayout = tw("aside")`lg:w-0 lg:flex-1`;
-const Title = tw(
-    "h2"
-)`text-3xl font-extrabold tracking-tight text-white sm:text-4xl text-gray-800`;
+const Title = tw("h2")`
+    text-3xl
+    font-extrabold
+    tracking-tight
+    text-white
+    sm:text-4xl
+    text-gray-800
+`;
 const Description = tw("p")`mt-3 max-w-3xl text-lg leading-6 text-gray-600`;
 
 const FormLayout = tw("aside")`mt-8 lg:mt-0 lg:ml-8`;
 const Form = tw("form")`sm:flex`;
 const Label = tw("label")`sr-only`;
-const Input = tw(
-    "input"
-)`w-full px-5 py-3 border border-transparent placeholder-gray-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white focus:border-white sm:max-w-xs rounded-md`;
-const ButtonLayout = tw(
-    "div"
-)`mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0`;
-const Button = tw(
-    "button"
-)`w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500`;
+const Input = tw("input")`
+    w-full
+    px-5
+    py-3
+    border
+    border-transparent
+    placeholder-gray-500
+    focus:ring-2
+    focus:ring-offset-2
+    focus:ring-offset-gray-800
+    focus:ring-white
+    focus:border-white
+    sm:max-w-xs rounded-md
+`;
+const ButtonLayout = tw("div")`
+    mt-3
+    rounded-md
+    shadow
+    sm:mt-0
+    sm:ml-3
+    sm:flex-shrink-0
+`;
+const Button = tw("button")`
+    w-full
+    flex
+    items-center
+    justify-center
+    px-5
+    py-3
+    border
+    border-transparent
+    text-base
+    font-medium
+    rounded-md
+    text-white
+    bg-indigo-500
+    hover:bg-indigo-600
+    focus:outline-none
+    focus:ring-2
+    focus:ring-offset-2
+    focus:ring-offset-gray-800
+    focus:ring-indigo-500
+`;
 const FormText = tw("p")`mt-3 text-sm text-gray-600`;
 const FormLink = tw("a")`text-white font-medium underline text-gray-600`;
 
