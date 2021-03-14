9774872ecc479c856650374b207a402628d222ba
tsarapoid <nadein@gmail.com>
2021-03-14 09:58:49 +0300

    revert babel config, ignore form eslint

---
+++ babel.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    presets: [
+        "@babel/preset-typescript",
+        [
+            "@babel/preset-env",
+            {
+                corejs: { version: 3, proposals: false },
+                useBuiltIns: "entry",
+                targets: {
+                    node: "current",
+                    browsers: [
+                        "Chrome >= 71",
+                        "iOS >= 11",
+                        "Safari >= 11",
+                        "Firefox >= 60",
+                    ],
+                },
+            },
+        ],
+    ],
+    plugins: [
+        "babel-plugin-macros",
+        "@babel/plugin-transform-react-jsx",
+        [
+            "babel-plugin-graphql-tag",
+            {
+                strip: true,
+            },
+        ],
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
+        ["@babel/plugin-proposal-class-properties", { loose: true }],
+        "@babel/plugin-transform-runtime",
+        // [
+        //     "@babel/plugin-proposal-object-rest-spread",
+        //     { loose: false, useBuiltIns: true }
+        // ],
+        "transform-react-remove-prop-types",
+        [
+            "emotion",
+            {
+                sourceMap: false,
+                autoLabel: false,
+                labelFormat: "[local]",
+                cssPropOptimization: true,
+            },
+        ],
+    ],
+};
