630e3ec50c45cbd07ad2b83b24fddc96cd42c06f
ninuvi <vik_ivy@mail.ru>
2021-03-16 15:15:20 +0300

    Add team block for main page

--- src/Layout/Sections/Team/index.js
+++ src/Layout/Sections/Team/index.js
@@ -16,72 +16,19 @@ const Team = () => (
                 <TeamLayout>
                     <TeamList>
                         <TeamMember>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Leslie Alexander
-                                    </MemberName>
-                                    <Position>
-                                        Co-Founder / CEO
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Michael Foster
-                                    </MemberName>
-                                    <Position>
-                                        Co-Founder / CTO
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Dries Vincent
-                                    </MemberName>
-                                    <Position>
-                                        Manager, Business Relations
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Lindsay Walton
-                                    </MemberName>
-                                    <Position>
-                                        Front-end Developer
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Courtney Henry
-                                    </MemberName>
-                                    <Position>
-                                        Designer
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
-                            <Member>
-                                <Img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
-                                <MemberInfo>
-                                    <MemberName>
-                                        Tom Cook
-                                    </MemberName>
-                                    <Position>
-                                        Director, Product Development
-                                    </Position>
-                                </MemberInfo>
-                            </Member>
+                            {members}.map((member) =>
+                                <Member>
+                                    <Img src={member.img} />
+                                    <MemberInfo>
+                                        <MemberName>
+                                            {member.name}
+                                        </MemberName>
+                                        <Position>
+                                            {member.position}
+                                        </Position>
+                                    </MemberInfo>
+                                </Member>
+                            )
                         </TeamMember>
                     </TeamList>
                 </TeamLayout>
@@ -89,6 +36,15 @@ const Team = () => (
         </Section>
     </Container>
 );
+
+const members = [
+    {img: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80',name: 'Leslie Alexander', position: 'Co-Founder / CEO'},
+    {img: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80', name: 'Michael Foster', position: 'Co-Founder / CTO'},
+    {img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80', name: 'Dries Vincent', position: 'Manager, Business Relations'},
+    {img: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80', name: 'Lindsay Walton', position: 'Front-end Developer'},
+    {img: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80', name: 'Courtney Henry', position: 'Designer'},
+    {img: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80', name: 'Tom Cook', position: 'Director, Product Development'}
+];
 
 const Container = tw("div")`bg-yellow-300`;
 const Section = tw("section")`mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24`;
