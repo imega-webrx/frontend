4a8e1366698728da1a604acc3caa1d9a53bfd078
tsarapoid <nadein@gmail.com>
2021-03-17 20:09:26 +0300

    add logo

--- src/Layout/NavBar/MenuBar/Logo.js
+++ src/Layout/NavBar/MenuBar/Logo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import tw from "twin.macro";
+import logo from "./logo.jpg";
 
 const Logo = () => (
     <Container>
-        <Img src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" />
+        <Img src={logo} alt="company logo" />
     </Container>
 );
 
 const Container = tw("div")`flex-shrink-0`;
-const Img = tw("img")`h-8 w-8`;
+const Img = tw("img")`h-12 w-12`;
 
 export default Logo;
