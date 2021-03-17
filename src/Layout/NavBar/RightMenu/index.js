d4b25421631e66c6cbb02d2a2d8769bcc67ffc44
tsarapoid <nadein@gmail.com>
2021-03-17 15:45:58 +0300

    change header

--- src/Layout/NavBar/RightMenu/index.js
+++ src/Layout/NavBar/RightMenu/index.js
@@ -31,78 +31,80 @@ const DropDownItem = tw(
 
 const RightMenu = () => (
     <React.Fragment>
-        <HeaderBar>
-            <RightMenuContainer>
-                <DropDownContainer>
-                    <BellIcon>
-                        <BellIconSpan>View notifications</BellIconSpan>
-                        <BellIconSVG
-                            data-todo-x-description="Heroicon name: outline/bell"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            aria-hidden="true"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                            ></path>
-                        </BellIconSVG>
-                    </BellIcon>
-
-                    {/* Profile dropdown */}
-                    <DropDownMenu
-                        data-todo-x-data="{ open: false }"
-                        data-todo-at-keydown-escape-stop="open = false"
-                        data-todo-at-click-away="open = false"
-                    >
-                        <div>
-                            <DropDownButton
-                                type="button"
-                                id="user-menu"
-                                aria-expanded="false"
-                                data-todo-at-click="open = !open"
-                                aria-haspopup="true"
-                                data-todo-x-bind-aria-expanded="open"
+        <HeaderContainer>
+            <HeaderBar>
+                <RightMenuContainer>
+                    <DropDownContainer>
+                        <BellIcon>
+                            <BellIconSpan>View notifications</BellIconSpan>
+                            <BellIconSVG
+                                data-todo-x-description="Heroicon name: outline/bell"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
                             >
-                                <DropDownSpan>Open user menu</DropDownSpan>
-                                <DropDownBell
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt=""
-                                />
-                            </DropDownButton>
-                        </div>
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                ></path>
+                            </BellIconSVG>
+                        </BellIcon>
 
-                        <DropDownList
-                            data-todo-x-description="Dropdown menu, show/hide based on menu state."
-                            data-todo-x-show="open"
-                            data-todo-x-transition-enter="transition ease-out duration-200"
-                            data-todo-x-transition-enter-start="transform opacity-0 scale-95"
-                            data-todo-x-transition-enter-end="transform opacity-100 scale-100"
-                            data-todo-x-transition-leave="transition ease-in duration-75"
-                            data-todo-x-transition-leave-start="transform opacity-100 scale-100"
-                            data-todo-x-transition-leave-end="transform opacity-0 scale-95"
-                            role="menu"
-                            aria-orientation="vertical"
-                            aria-labelledby="user-menu"
+                        {/* Profile dropdown */}
+                        <DropDownMenu
+                            data-todo-x-data="{ open: false }"
+                            data-todo-at-keydown-escape-stop="open = false"
+                            data-todo-at-click-away="open = false"
                         >
-                            <DropDownItem href="/" role="menuitem">
-                                Your Profile
-                            </DropDownItem>
-                            <DropDownItem href="/" role="menuitem">
-                                Settings
-                            </DropDownItem>
-                            <DropDownItem href="/" role="menuitem">
-                                Sign out
-                            </DropDownItem>
-                        </DropDownList>
-                    </DropDownMenu>
-                </DropDownContainer>
-            </RightMenuContainer>
-        </HeaderBar>
+                            <div>
+                                <DropDownButton
+                                    type="button"
+                                    id="user-menu"
+                                    aria-expanded="false"
+                                    data-todo-at-click="open = !open"
+                                    aria-haspopup="true"
+                                    data-todo-x-bind-aria-expanded="open"
+                                >
+                                    <DropDownSpan>Open user menu</DropDownSpan>
+                                    <DropDownBell
+                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        alt=""
+                                    />
+                                </DropDownButton>
+                            </div>
+
+                            <DropDownList
+                                data-todo-x-description="Dropdown menu, show/hide based on menu state."
+                                data-todo-x-show="open"
+                                data-todo-x-transition-enter="transition ease-out duration-200"
+                                data-todo-x-transition-enter-start="transform opacity-0 scale-95"
+                                data-todo-x-transition-enter-end="transform opacity-100 scale-100"
+                                data-todo-x-transition-leave="transition ease-in duration-75"
+                                data-todo-x-transition-leave-start="transform opacity-100 scale-100"
+                                data-todo-x-transition-leave-end="transform opacity-0 scale-95"
+                                role="menu"
+                                aria-orientation="vertical"
+                                aria-labelledby="user-menu"
+                            >
+                                <DropDownItem href="/" role="menuitem">
+                                    Your Profile
+                                </DropDownItem>
+                                <DropDownItem href="/" role="menuitem">
+                                    Settings
+                                </DropDownItem>
+                                <DropDownItem href="/" role="menuitem">
+                                    Sign out
+                                </DropDownItem>
+                            </DropDownList>
+                        </DropDownMenu>
+                    </DropDownContainer>
+                </RightMenuContainer>
+            </HeaderBar>
+        </HeaderContainer>
     </React.Fragment>
 );
 
