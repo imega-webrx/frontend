4a86ba40c34edfb7e3504bf3fa88a932de2e6fcf
ninuvi <vik_ivy@mail.ru>
2021-03-19 13:41:45 +0300

    Refactored navbar

--- src/Layout/NavBar/MenuBar/index.js
+++ src/Layout/NavBar/MenuBar/index.js
@@ -13,7 +13,7 @@ const Main = ({ children }) => (
 );
 
 const MenuBar = tw("div")`flex items-center`;
-const Container = tw("div")`hidden md:block`;
+const Container = tw("div")`hidden md:ml-6 md:flex md:items-center md:space-x-4`;
 const Column = tw("div")`ml-10 flex items-center space-x-4`;
 
 export default Main;
