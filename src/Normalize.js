4794008c5855136a5fdb2469e6f57aa25fb7e51f
ninuvi <vik_ivy@mail.ru>
2021-03-19 17:09:36 +0300

    Added some css

--- src/Normalize.js
+++ src/Normalize.js
@@ -30,6 +30,27 @@ const Normalize = () => (
                 color: inherit;
                 text-decoration: inherit;
             }
+            .rounded {
+                border-radius: 0.25rem;
+            }
+            .rounded-md {
+                border-radius: 0.375rem;
+            }
+            .pl-2 {
+                padding-left: 0.5rem;
+            }
+            .border-indigo-500 {
+                --tw-border-opacity: 1;
+                border-color: rgba(99, 102, 241, var(--tw-border-opacity));
+            }
+            .ring-indigo-500:focus {
+                --tw-ring-opacity: 1;
+                --tw-ring-color: rgba(99, 102, 241, var(--tw-ring-opacity));
+            }
+            .focus:border-indigo-500:focus {
+                --tw-border-opacity: 1;
+                border-color: rgba(99, 102, 241, var(--tw-border-opacity));
+            }
         `}
     />
 );
