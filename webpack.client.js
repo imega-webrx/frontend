1ea0571f2da83f005f35d8d9b7435d077072e671
ninuvi <vik_ivy@mail.ru>
2021-04-04 21:32:27 +0300

    Add webpack for client

---
+++ webpack.client.js
@@ -0,0 +1,40 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: {
+        client: "./src/Layout/NavBar/MobileMenu/index.js"
+    },
+    target: "web",
+    mode: "production",
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|mjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    ie8: false,
+                    output: {
+                        comments: false
+                    }
+                },
+                sourceMap: true,
+                extractComments: false
+            })
+        ]
+    }
+};
