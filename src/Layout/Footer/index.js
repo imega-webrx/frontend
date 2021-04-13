834734ce05968b36703673925ba01f3f318c0edc
ZhenyaDope <zhenyadope@gmail.com>
2021-04-13 10:07:56 +0300

    fix: format error

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -3,114 +3,117 @@ import tw from "twin.macro";
 
 // images
 import images from "./img";
-const {fb,dribble,twitter,github,inst,logo} = images;
+const { fb, dribble, twitter, github, inst, logo } = images;
 
 const Footer = () => (
     <Container>
         <Section className="container">
             <SocialBlock>
                 <Logo>
-                    <Img src={logo}/>
+                    <Img src={logo} />
                 </Logo>
-                <JustText>Making the world a better place through constructing elegant hierarchies.</JustText>
+                <JustText>
+                    Making the world a better place through constructing elegant
+                    hierarchies.
+                </JustText>
                 <SocialLinks>
                     <SocialLinkItem>
                         <Link href="/">
-                            <Img src={fb}/>
+                            <Img src={fb} />
                         </Link>
                     </SocialLinkItem>
                     <SocialLinkItem>
                         <Link href="/">
-                            <Img src={inst}/>
+                            <Img src={inst} />
                         </Link>
                     </SocialLinkItem>
                     <SocialLinkItem>
                         <Link href="/">
-                            <Img src={twitter}/>
+                            <Img src={twitter} />
                         </Link>
                     </SocialLinkItem>
                     <SocialLinkItem>
                         <Link href="/">
-                            <Img src={github}/>
+                            <Img src={github} />
                         </Link>
                     </SocialLinkItem>
                     <SocialLinkItem>
                         <Link href="/">
-                            <Img src={dribble}/>
+                            <Img src={dribble} />
                         </Link>
                     </SocialLinkItem>
                 </SocialLinks>
             </SocialBlock>
-                <PagesBlock>
-                    <PagesBlockItem>
-                        <PageListTitle>Solutions</PageListTitle>
-                            <PagesList>
-                                <PageListItem>
-                                    <Link href="/">Marketing</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Analytics</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Commerce</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Insights</Link>
-                                </PageListItem>
-                            </PagesList>
-                    </PagesBlockItem>
-                    <PagesBlockItem>
-                        <PageListTitle>Support</PageListTitle>
-                            <PagesList>
-                                <PageListItem>
-                                    <Link href="/">Pricing</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Documentation</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Guides</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">API Status</Link>
-                                </PageListItem>
-                            </PagesList>
-                    </PagesBlockItem>
-                    <PagesBlockItem>
-                        <PageListTitle>Company</PageListTitle>
-                            <PagesList>
-                                <PageListItem>
-                                    <Link href="/">About</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Blog</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Jobs</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Press</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Partners</Link>
-                                </PageListItem>
-                            </PagesList>
-                    </PagesBlockItem>
-                    <PagesBlockItem>
-                        <PageListTitle>Legal</PageListTitle>
-                            <PagesList>
-                                <PageListItem>
-                                    <Link href="/">Claim</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Privacy</Link>
-                                </PageListItem>
-                                <PageListItem>
-                                    <Link href="/">Terms</Link>
-                                </PageListItem>
-                            </PagesList>
-                    </PagesBlockItem>
-                </PagesBlock>   
+            <PagesBlock>
+                <PagesBlockItem>
+                    <PageListTitle>Solutions</PageListTitle>
+                    <PagesList>
+                        <PageListItem>
+                            <Link href="/">Marketing</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Analytics</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Commerce</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Insights</Link>
+                        </PageListItem>
+                    </PagesList>
+                </PagesBlockItem>
+                <PagesBlockItem>
+                    <PageListTitle>Support</PageListTitle>
+                    <PagesList>
+                        <PageListItem>
+                            <Link href="/">Pricing</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Documentation</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Guides</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">API Status</Link>
+                        </PageListItem>
+                    </PagesList>
+                </PagesBlockItem>
+                <PagesBlockItem>
+                    <PageListTitle>Company</PageListTitle>
+                    <PagesList>
+                        <PageListItem>
+                            <Link href="/">About</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Blog</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Jobs</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Press</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Partners</Link>
+                        </PageListItem>
+                    </PagesList>
+                </PagesBlockItem>
+                <PagesBlockItem>
+                    <PageListTitle>Legal</PageListTitle>
+                    <PagesList>
+                        <PageListItem>
+                            <Link href="/">Claim</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Privacy</Link>
+                        </PageListItem>
+                        <PageListItem>
+                            <Link href="/">Terms</Link>
+                        </PageListItem>
+                    </PagesList>
+                </PagesBlockItem>
+            </PagesBlock>
         </Section>
         <Copyright>
             <JustText>© 2020 Workflow, Inc. All rights reserved.</JustText>
@@ -132,8 +135,8 @@ const Section = tw("div")`
     sm:pt-24
     sm:px-6
     lg:pt-16 pb-12
-    border-b	
-    border-gray-300	
+    border-b
+    border-gray-300
     mb-4
     w-full
     flex-wrap
@@ -170,14 +173,14 @@ const PagesBlock = tw("div")`
 `;
 const PagesList = tw("ul")`
     flex
-    flex-col   
+    flex-col
 `;
 const PageListTitle = tw("h5")`
     font-bold
     uppercase
     text-gray-400
     mb-4
-    text-sm	
+    text-sm
 `;
 const PagesBlockItem = tw("div")`
     flex
@@ -185,13 +188,14 @@ const PagesBlockItem = tw("div")`
     mb-8
     sm:mr-10
     lg:mr-28
-    mr-10   
+    mr-10
 `;
 const PageListItem = tw("li")`
     mb-4 last:mb-0
     text-gray-300
-    text-base	
+    text-base
 `;
+
 const Copyright = tw("div")`
     mx-auto
     mt-4
@@ -201,5 +205,4 @@ const Copyright = tw("div")`
     justify-center
     text-gray-300
 `;
-
 export default Footer;
