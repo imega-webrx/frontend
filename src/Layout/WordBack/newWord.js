b1c2734196b511f023b98961b938ade077e2aa8b
Danila-hub <Danila-hub@users.noreply.github.com>
2021-05-29 12:53:19 +0000

    Prettified Code!

--- src/Layout/WordBack/newWord.js
+++ src/Layout/WordBack/newWord.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { ApolloProvider, ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+    ApolloProvider,
+    ApolloClient,
+    ApolloLink,
+    HttpLink,
+    InMemoryCache,
+} from "@apollo/client";
 
 function ShowWords() {
     const BigWord = styled.div`
-    font-size: 60px;
-    text-align: center;
-    margin-top: 50px;
-    `
+        font-size: 60px;
+        text-align: center;
+        margin-top: 50px;
+    `;
     const client = new ApolloClient({
         link: ApolloLink.from([HttpLink]),
         cache: new InMemoryCache(),
